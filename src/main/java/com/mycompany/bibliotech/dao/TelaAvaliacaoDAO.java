2f00326064f29ffd41d09aa0256578f1b281fb20
Sérgio Beltrão <sergio.bt.souza@gmail.com>
2023-11-22 03:54:24 -0300

    feat(gui): funcao de buscas implementada na tela de teste

--- src/main/java/com/mycompany/bibliotech/dao/TelaAvaliacaoDAO.java
+++ src/main/java/com/mycompany/bibliotech/dao/TelaAvaliacaoDAO.java
@@ -9,8 +9,9 @@ import java.sql.SQLException;
 import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 
-
 public class TelaAvaliacaoDAO {
+
+    // Mostrar todos os livros. Sem filtrar.
     public static void listaTituloDosLivros(JComboBox<String> comboBox) {
         try {
             Connection con = ConnectionFactory.getConnection();
@@ -19,14 +20,41 @@ public class TelaAvaliacaoDAO {
             ResultSet resultado = comando.executeQuery();
 
             comboBox.removeAllItems();
-            comboBox.addItem("Não Informada");
+            comboBox.addItem("Resultados...");
 
             while (resultado.next()) {
                 comboBox.addItem(resultado.getString("LIV_NOME_LIVRO"));
             }
         } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Erro ao ler a tabela de nacionalidades: " + ex);
+            JOptionPane.showMessageDialog(null, "Erro ao ler a tabela de livros: " + ex);
         }
     }
-    
+
+    // Mostra os livros com base no que foi digitado.
+    public static void listaFiltrada(JComboBox<String> comboBox, String busca) {
+
+        try {
+            Connection con = ConnectionFactory.getConnection();
+            PreparedStatement stmt = null;
+
+            stmt = con.prepareStatement("SELECT LIV_NOME_LIVRO FROM LIVRO WHERE LIV_NOME_LIVRO LIKE ?");
+
+            // Tive que mandar os % do comando SQL pra cá. Se usar direto no prepareStatement vai
+            // dar o erro "parameter index out of range (1 number of parameters which is 0)"
+            
+            stmt.setString(1, "%" + busca + "%");
+
+            //PreparedStatement comando = con.prepareStatement(sql);
+            ResultSet resultado = stmt.executeQuery();
+
+            comboBox.removeAllItems();
+            comboBox.addItem("Resultados...");
+
+            while (resultado.next()) {
+                comboBox.addItem(resultado.getString("LIV_NOME_LIVRO"));
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao ler a tabela de livros: " + ex);
+        }
+    }
 }
--- src/main/java/com/mycompany/bibliotech/telas/Teste.java
+++ src/main/java/com/mycompany/bibliotech/telas/Teste.java
@@ -1,53 +1,112 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package com.mycompany.bibliotech.telas;
 
 import com.mycompany.bibliotech.dao.TelaAvaliacaoDAO;
-
+import javax.swing.JOptionPane;
 
 public class Teste extends javax.swing.JFrame {
 
     public Teste() {
         initComponents();
-        listarBusca();
+        // Lembre-se de descomentar isso se quiser usar o método "listaTituloDosLivros"
+        // listarBusca();
     }
 
-        public void listarBusca(){
-        TelaAvaliacaoDAO.listaTituloDosLivros(cboxTeste);
+    // Você só precisa disso para listar TODOS os livros (sem usar a busca).
+    // Usa o método "listaTituloDosLivros" dentro do TelaAvaliacaoDAO.
+    public void listarBusca() {
+        //  String busca = txtTeste.getText();
+        // TelaAvaliacaoDAO.listaFiltrada(cboxTeste, busca);
     }
-    
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        panTeste = new javax.swing.JPanel();
+        txtTeste = new javax.swing.JTextField();
         cboxTeste = new javax.swing.JComboBox<>();
+        btnBuscar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        cboxTeste.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Selecione", " " }));
+        panTeste.setBackground(new java.awt.Color(255, 255, 255));
+
+        txtTeste.setBorder(javax.swing.BorderFactory.createTitledBorder("Digite o título do livro"));
+        txtTeste.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtTesteActionPerformed(evt);
+            }
+        });
+
+        btnBuscar.setText("Buscar");
+        btnBuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBuscarActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout panTesteLayout = new javax.swing.GroupLayout(panTeste);
+        panTeste.setLayout(panTesteLayout);
+        panTesteLayout.setHorizontalGroup(
+            panTesteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, panTesteLayout.createSequentialGroup()
+                .addContainerGap(70, Short.MAX_VALUE)
+                .addGroup(panTesteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(btnBuscar)
+                    .addComponent(cboxTeste, javax.swing.GroupLayout.PREFERRED_SIZE, 350, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtTeste, javax.swing.GroupLayout.PREFERRED_SIZE, 350, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(70, 70, 70))
+        );
+        panTesteLayout.setVerticalGroup(
+            panTesteLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panTesteLayout.createSequentialGroup()
+                .addGap(50, 50, 50)
+                .addComponent(txtTeste, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(cboxTeste, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(btnBuscar)
+                .addContainerGap(50, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(44, 44, 44)
-                .addComponent(cboxTeste, javax.swing.GroupLayout.PREFERRED_SIZE, 330, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(26, Short.MAX_VALUE))
+            .addComponent(panTeste, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(90, 90, 90)
-                .addComponent(cboxTeste, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(163, Short.MAX_VALUE))
+            .addComponent(panTeste, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBuscarActionPerformed
+
+        // O que faz a mágica acontecer é isso. O restante é no TelaAvaliacaoDAO.
+        String busca = txtTeste.getText();
+
+        // Aqui eu verifico o tamanho do texto que foi digitado. Se for menor que 1
+        // uma mensagem aparerá pedindo para o usuário digitar algo. Eu poderia ter colocado
+        // igual a zero (já que menos que 1 é 0) mas vou deixar assim. Acredito que seja
+        // bacana limitar o número mínimo de caractéries para fazer uma busca (quem sabe uns
+        // três ou quatro). Fica ao seu critério quando for implementar. A mensagem também
+        // pode ser melhorada.
+        if (busca.length() < 1) {
+            JOptionPane.showMessageDialog(null, "Digite algo!");
+            cboxTeste.removeAllItems(); // Limba a ComboBox se tentar pesquisar sem texto
+            // depois de ter pesquisado com.
+        } else {
+            TelaAvaliacaoDAO.listaFiltrada(cboxTeste, busca);
+        }
+    }//GEN-LAST:event_btnBuscarActionPerformed
+
+    private void txtTesteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtTesteActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtTesteActionPerformed
 
     /**
      * @param args the command line arguments
@@ -60,7 +119,7 @@ public class Teste extends javax.swing.JFrame {
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Windows".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
@@ -85,6 +144,9 @@ public class Teste extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnBuscar;
     private javax.swing.JComboBox<String> cboxTeste;
+    private javax.swing.JPanel panTeste;
+    private javax.swing.JTextField txtTeste;
     // End of variables declaration//GEN-END:variables
 }
