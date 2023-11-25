7e168620ea2789c870a08abf0066f391dea8f2e2
Bruno Francisco <brunoffco@gmail.com>
2023-11-24 22:12:19 -0300

    feat(eduser): itens obrigatorios marcados com *

--- src/main/java/com/mycompany/bibliotech/telas/EdicaoUsuario.java
+++ src/main/java/com/mycompany/bibliotech/telas/EdicaoUsuario.java
@@ -192,7 +192,7 @@ public class EdicaoUsuario extends javax.swing.JFrame {
             }
         });
 
-        dataNascChooser.setBorder(javax.swing.BorderFactory.createTitledBorder("Data de Nascimento"));
+        dataNascChooser.setBorder(javax.swing.BorderFactory.createTitledBorder("Data de Nascimento *"));
         dataNascChooser.setToolTipText("2004-08-07");
         dataNascChooser.setDateFormatString("yyyy-MM-dd");
 
