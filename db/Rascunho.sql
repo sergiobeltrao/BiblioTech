e85fe5017b37f90bb7543bb1d4b9ef689704003a
Jonatas Santos <jonatas918@gmail.com>
2023-11-25 01:46:42 -0300

    feat(database): oraganizacao

--- db/Rascunho.sql
+++ db/Rascunho.sql
@@ -23,6 +23,44 @@ INSERT INTO AUTOR ( AUT_NOME_AUTOR, AUT_NACIONALIDADE, AUT_SEXO) VALUES
 ('George R. R. Martin', 'Estados Unidos - Americana', 'MASC' ),
 ('Agatha Christie', 'Reino Unido - Britânica', 'FEM');
 
+-- livros
+INSERT INTO LIVRO (LIV_NOME_LIVRO, LIV_ISBN, LIV_ANO, LIV_PAGINA, LIV_CATEGORIA, LIV_SUBCATEGORIA, LIV_IDIOMA, LIV_EDITORA, LIV_SINOPSE)
+VALUES ('Dom Casmurro', '9788532820480', 1899, 256, 'Ficção', 'Romance', 'Português', 'Editora Nova Fronteira', 'A obra retrata a vida de Bento Santiago, um homem enigmático e melancólico.'),
+('Cosmos', '9780345539434', 1980, 384, 'Não Ficção', 'Ciências', 'Inglês', 'Ballantine Books', 'Uma jornada pelo universo, explorando ciência, filosofia e a história da vida na Terra.'),
+('O Pequeno Príncipe', '9788576573195', 1943, 96, 'Literatura Infantil e Juvenil', 'Contos de Fadas', 'Francês', 'Gallimard', 'Uma história encantadora sobre amizade e valores essenciais da vida.'),
+('A Origem das Espécies', '9788533616655', 1859, 480, 'Não Ficção', 'Ciências', 'Português', 'L&PM Editores', 'Charles Darwin explora a teoria da evolução e a seleção natural.'),
+('O Poder do Hábito', '9788539004115', 2012, 408, 'Não Ficção', 'Autoajuda', 'Português', 'Objetiva', 'Investiga como os hábitos funcionam e como podem ser transformados.'),
+('Cem Anos de Solidão', '9788535908064', 1967, 448, 'Ficção', 'Romance', 'Espanhol', 'Editora Record', 'A história da família Buendía ao longo de várias gerações em Macondo.'),
+('A Revolução dos Bichos', '9788535907708', 1945, 128, 'Ficção', 'Sátira', 'Português', 'Companhia das Letras', 'Uma alegoria sobre os eventos que levaram à Revolução Russa.'),
+('O Código Da Vinci', '9788575428769', 2003, 448, 'Ficção', 'Mistério', 'Português', 'Editora Arqueiro', 'Uma trama envolvente de conspirações e mistérios ligados à Igreja Católica.'),
+('A Arte da Guerra', '9788533618819', 0001, 112, 'Não Ficção', 'Filosofia', 'Português', 'L&PM Pocket', 'Um tratado militar atribuído a Sun Tzu sobre estratégias de guerra.'),
+('O Senhor dos Anéis: A Sociedade do Anel', '9788578271223', 1954, 576, 'Ficção', 'Fantasia', 'Inglês', 'Martins Fontes', 'A primeira parte da trilogia épica de J.R.R. Tolkien.');
+
+-- Autores
+INSERT INTO AUTOR (AUT_NOME_AUTOR, AUT_NACIONALIDADE, AUT_SEXO)
+VALUES ('Machado de Assis', 'Brasileira', 'MASC'), ('Carl Sagan', 'Estadunidense', 'MASC'),
+('Antoine de Saint-Exupéry', 'Francês', 'MASC'), ('Charles Darwin', 'Britânica', 'MASC'),
+('Charles Duhigg', 'Estadunidense', 'MASC'), ('Gabriel García Márquez', 'Colombiana', 'MASC'),
+('George Orwell', 'Britânica', 'MASC'), ('Dan Brown', 'Estadunidense', 'MASC'),
+('Sun Tzu', 'Chinesa', 'MASC'), ('J.R.R. Tolkien', 'Britânica', 'MASC');
+
+-- chave estrangeira Livros e Autores
+INSERT INTO LIVRO_AUTOR (LIVRO_CHAVE, LIVRO_AUTOR)
+VALUES (1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (9, 9), (10, 10);  
+
+-- Avaliações dos Livros
+-- O INT usado no AVA_ID_USUARIO é i ID do usuario da tabela usuario, o mesmo se aplica ao AVA_FK_LIVRO
+INSERT INTO AVALIACAO (AVA_ID_USUARIO, AVA_FK_LIVRO, AVA_USUARIO, AVA_TOTAL, AVA_COMENTARIO)
+VALUES (1, 1, 9.0, 8.5, 'Um clássico da literatura brasileira.'), (2, 2, 7.8, 7.5, 'Fascinante jornada pelo cosmos.'),
+(3, 3, 8, 8.0, 'Uma história encantadora para todas as idades.');
+
+/* 
+estes so vão funcionar quando tiverem no miniumo 10 usuarios inseridos no banco
+(4, 4, 9, 9.0, 'Obra fundamental para entender a evolução.'),
+(5, 5, 7, 7.0, 'Interessante abordagem sobre o poder dos hábitos.'), (6, 6, 9, 9.5, 'Uma obra-prima da literatura latino-americana.'),
+(7, 7, 8, 8.0, 'Uma distopia que permanece relevante.'), (8, 8, 7, 7.5, 'Thriller intrigante com códigos e simbologias.'),
+(9, 9, 8, 8.5, 'Ensinamentos atemporais sobre estratégia.'), (10, 10, 9, 9.7, 'Um épico de fantasia que marcou gerações.'); 
+*/
 
 /*INSERT DOS VINCULOS DE ENDEREÇO E TELEFONE */
 INSERT INTO TELEFONE_USUARIO VALUES (3, 2), (1, 1), (2, 3);
