fab1b7b3c55489ea2f0519edce76a315eb74679f
Sérgio Beltrão <sergio.bt.souza@gmail.com>
2023-11-27 16:11:36 -0300

    feat(gui): tela de hanking por numero de notas implementada

---
+++ src/main/java/com/mycompany/bibliotech/model/bean/ContagemAvaliacoesLivro.java
@@ -0,0 +1,24 @@
+package com.mycompany.bibliotech.model.bean;
+
+public class ContagemAvaliacoesLivro {
+
+    private String nomeLivro;
+    private int quantidadeAvaliacoes;
+
+    public String getNomeLivro() {
+        return nomeLivro;
+    }
+
+    public void setNomeLivro(String nomeLivro) {
+        this.nomeLivro = nomeLivro;
+    }
+
+    public int getQuantidadeAvaliacoes() {
+        return quantidadeAvaliacoes;
+    }
+
+    public void setQuantidadeAvaliacoes(int quantidadeAvaliacoes) {
+        this.quantidadeAvaliacoes = quantidadeAvaliacoes;
+    }
+
+}
