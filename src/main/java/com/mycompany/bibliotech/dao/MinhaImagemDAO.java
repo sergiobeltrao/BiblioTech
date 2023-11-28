856a08c6e32c59f6d6ca8af699472e56b5a5cc70
Sérgio Beltrão <sergio.bt.souza@gmail.com>
2023-11-28 03:40:14 -0300

    feat(gui) exemplo de insert de imagens

---
+++ src/main/java/com/mycompany/bibliotech/dao/MinhaImagemDAO.java
@@ -0,0 +1,107 @@
+package com.mycompany.bibliotech.dao;
+
+import com.mycompany.bibliotech.connection.ConnectionFactory;
+import com.mycompany.bibliotech.model.bean.MinhaImagem;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+
+public class MinhaImagemDAO {
+
+    // NO BANCO EU CRIEI UMA TABELA DE TESTES. MEU CÓDIGO SQL...
+    // USE BIBLIOTECH;
+    // CREATE TABLE TB_TESTE(ID INT PRIMARY KEY AUTO_INCREMENT, NOME VARCHAR(50), IMAGEM LONGBLOB);
+    public void inserirImagem(MinhaImagem imagem) throws SQLException {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        String sql = "INSERT INTO TB_TESTE (NOME, IMAGEM) VALUES (?, ?)";
+
+        try {
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, imagem.getNome());
+            stmt.setBytes(2, imagem.getImagem());
+            stmt.execute();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(MinhaImagem.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void excluirImagem(MinhaImagem imagem) {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        String sql = "DELETE FROM TB_TESTE WHERE ID = ?";
+
+        try {
+            stmt = con.prepareStatement(sql);
+            stmt.setInt(1, imagem.getId());
+            stmt.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(MinhaImagem.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void alterarImagem(MinhaImagem imagem) {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        String sql = "UPDATE TB_TESTE SET NOME = ?, IMAGEM = ? WHERE ID = ?";
+
+        try {
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, imagem.getNome());
+            stmt.setBytes(2, imagem.getImagem());
+            stmt.setInt(3, imagem.getId());
+            stmt.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(MinhaImagem.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void listarImagens(JComboBox<String> comboBox) {
+        try {
+            Connection con = ConnectionFactory.getConnection();
+            String sql = "SELECT * FROM TB_TESTE";
+            PreparedStatement comando = con.prepareStatement(sql);
+            ResultSet resultado = comando.executeQuery();
+
+            comboBox.removeAllItems();
+            comboBox.addItem("Selecione");
+
+            while (resultado.next()) {
+                comboBox.addItem(String.valueOf(resultado.getInt("ID")) + " - " + resultado.getString("NOME"));
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao ler a tabela de imagem: " + ex);
+        }
+    }
+
+    public byte[] buscarImagemPorId(int id) {
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        String sql = "SELECT IMAGEM FROM TB_TESTE WHERE ID = ?";
+        byte[] imagemBytes = null;
+
+        try {
+            stmt = con.prepareStatement(sql);
+            stmt.setInt(1, id);
+            rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                imagemBytes = rs.getBytes("IMAGEM");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(MinhaImagemDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            ConnectionFactory.closeConnection(con, stmt, rs);
+        }
+
+        return imagemBytes;
+    }
+
+}
---
+++ src/main/java/com/mycompany/bibliotech/model/bean/MinhaImagem.java
@@ -0,0 +1,32 @@
+package com.mycompany.bibliotech.model.bean;
+
+public class MinhaImagem {
+
+    int id;
+    String nome;
+    byte[] imagem;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public byte[] getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(byte[] imagem) {
+        this.imagem = imagem;
+    }
+}
